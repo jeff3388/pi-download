534c03b116f49fd742babeac0b7ca87ecad9d3ee
jojo <jaxlouder@gmail.com>
2019-07-19 16:08:02 +0800

    '123'

--- install_modlue.sh
+++ install_modlue.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
-sudo su - 
-cd /tmp 
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh
-/bin/bash /tmp/Miniconda3-latest-Linux-armv7l.sh 
-source ~/.bashrc
-echo '/opt/miniconda3/lib' >> /etc/ld.so.conf.d/miniconda.conf
-ldconfig -v
-conda config --add channels rpi
-conda install python=3.6 
-exit
-echo 'export PATH="/opt/miniconda3/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc 
+sudo apt-get install openssl libssl-dev
+sudo apt-get install libbz2-dev
+sudo apt-get install libncurses5-dev
+sudo apt-get install libncursesw5-dev
+sudo apt-get install libdb-dev
+sudo apt-get install libgdbm-dev
+sudo apt-get install liblzma-dev
+sudo apt-get install libsqlite3-dev
+sudo apt-get install tk-dev
+sudo apt-get install libreadline-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install libexpat1-dev
+wget https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz
+tar -zxvf Python-3.5.0.tgz
+cd Python-3.5.0
+./configure
+make
+sudo make install
 
 pip install beautifulsoup4
 pip install fake-useragent
