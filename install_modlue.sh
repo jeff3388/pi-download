e87fa4892e422f729aec918948167b380d34dee1
jojo <jaxlouder@gmail.com>
2019-07-19 15:04:43 +0800

    '123'

--- install_modlue.sh
+++ install_modlue.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev
-wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
-tar zxvf Python-3.6.1.tgz
-cd Python-3.6.1
-sudo ./configure && sudo make && sudo make install
-sudo mv /usr/bin/python /usr/bin/python3.4.2
-sudo ln -s /usr/local/bin/python3 /usr/bin/python
+sudo su - 
+cd /tmp 
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh
+/bin/bash /tmp/Miniconda3-latest-Linux-armv7l.sh 
+source ~/.bashrc
+echo '/opt/miniconda3/lib' >> /etc/ld.so.conf.d/miniconda.conf
+ldconfig -v
+conda config --add channels rpi
+conda install python=3.6 
+exit
+echo 'export PATH="/opt/miniconda3/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc 
 
 pip install beautifulsoup4
 pip install fake-useragent
